b10d22062d7330c77db3325e3d8c59eb249b3cff
Bob McWhirter <bob@fnokd.com>
2008-09-22 14:05:47 -0400

    Adding licensing, cleaning up some debug logging.

--- src/main/java/org/jboss/rails/deploy/RailsContextConfig.java
+++ src/main/java/org/jboss/rails/deploy/RailsContextConfig.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.rails.deploy;
 
 import org.apache.catalina.Wrapper;
@@ -10,8 +31,6 @@ import org.jboss.rails.metadata.RailsMetaData;
 
 public class RailsContextConfig extends ContextConfig {
 
-	// public static ThreadLocal<DeployerConfig> deployerConfig = new
-	// ThreadLocal<DeployerConfig>();
 	public static ThreadLocal<RailsMetaData> railsMetaData = new ThreadLocal<RailsMetaData>();
 
 	private static Logger log = Logger.getLogger(RailsContextConfig.class);
--- src/main/java/org/jboss/rails/deploy/RailsModule.java
+++ src/main/java/org/jboss/rails/deploy/RailsModule.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.rails.deploy;
 
 import org.jboss.deployers.vfs.spi.structure.VFSDeploymentUnit;
@@ -5,12 +26,13 @@ import org.jboss.logging.Logger;
 import org.jboss.rails.metadata.RailsMetaData;
 
 public class RailsModule implements RailsModuleMBean {
+
+	private Logger log = Logger.getLogger(RailsModule.class);
 	
-	private Logger log = Logger.getLogger( RailsModule.class );
 	private VFSDeploymentUnit unit;
 	private RailsAppDeployer deployer;
 	private RailsDeployment deployment;
-	
+
 	public RailsModule(VFSDeploymentUnit unit, RailsAppDeployer deployer, RailsDeployment deployment) {
 		this.unit = unit;
 		this.deployer = deployer;
@@ -18,21 +40,28 @@ public class RailsModule implements RailsModuleMBean {
 	}
 
 	public void create() throws Exception {
-		log.debug( "create()" );
+		if (log.isTraceEnabled()) {
+			log.trace("create()");
+		}
 	}
 
 	public void destroy() throws Exception {
-		log.debug( "destroy()" );
+		if (log.isTraceEnabled()) {
+			log.trace("destroy()");
+		}
 	}
 
 	public void start() throws Exception {
-		log.debug( "start() ish" );
-		log.debug( "starting deployment" );
-		deployment.start( unit.getAttachment( RailsMetaData.class ));
+		if (log.isTraceEnabled()) {
+			log.trace("start()");
+		}
+		deployment.start(unit.getAttachment(RailsMetaData.class));
 	}
 
 	public void stop() throws Exception {
-		log.debug( "stop()" );
+		if (log.isTraceEnabled()) {
+			log.trace("stop()");
+		}
 		deployment.stop();
 	}
 
