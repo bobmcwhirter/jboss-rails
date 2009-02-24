baba0ed8f624aa3b8e3e58f42f65ce0a0f433250
Bob McWhirter <bob@fnokd.com>
2009-02-24 18:25:00 -0500

    Inject the JBoss Kernel into the Ruby runtime during creation. Likewise, inject the Kernel into the deployer.

--- src/main/java/org/jboss/ruby/runtime/DefaultRubyRuntimeFactory.java
+++ src/main/java/org/jboss/ruby/runtime/DefaultRubyRuntimeFactory.java
@@ -1,13 +1,14 @@
 package org.jboss.ruby.runtime;
 
 import java.io.PrintStream;
-import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.jboss.kernel.Kernel;
 import org.jboss.logging.Logger;
 import org.jruby.Ruby;
+import org.jruby.RubyClass;
 import org.jruby.RubyInstanceConfig;
 import org.jruby.javasupport.JavaEmbedUtils;
 
@@ -15,6 +16,7 @@ public class DefaultRubyRuntimeFactory implements RubyRuntimeFactory {
 	
 	private static final Logger log = Logger.getLogger( DefaultRubyRuntimeFactory.class );
 
+	private Kernel kernel;
 	private List<String> loadPaths = null;
 	private String initScript = null;
 
@@ -35,6 +37,14 @@ public class DefaultRubyRuntimeFactory implements RubyRuntimeFactory {
 		}
 		this.initScript = initScript;
 		
+	}
+	
+	public void setKernel(Kernel kernel) {
+		this.kernel = kernel;
+	}
+	
+	public Kernel getKernel() {
+		return this.kernel;
 	}
 	
 	public void setClassLoader(RubyDynamicClassLoader classLoader) {
@@ -68,9 +78,16 @@ public class DefaultRubyRuntimeFactory implements RubyRuntimeFactory {
 		if ( this.initScript != null ) {
 			runtime.evalScriptlet( this.initScript );
 		}
+		injectKernel(runtime);
 		return runtime;
 	}
 	
+	private void injectKernel(Ruby runtime) {
+		runtime.evalScriptlet( "require %q(jboss/kernel)" );
+		RubyClass jbossKernel = (RubyClass) runtime.getClassFromPath( "JBoss::Kernel" );
+		JavaEmbedUtils.invokeMethod(runtime, jbossKernel, "kernel=", new Object[] { this.kernel }, void.class );
+	}
+
 	public Map<Object,Object> getEnvironment() {
 		return Collections.emptyMap();
 	}
--- src/main/java/org/jboss/ruby/runtime/deployers/RubyRuntimeFactoryDeployer.java
+++ src/main/java/org/jboss/ruby/runtime/deployers/RubyRuntimeFactoryDeployer.java
@@ -6,6 +6,7 @@ import org.jboss.deployers.spi.DeploymentException;
 import org.jboss.deployers.spi.deployer.DeploymentStages;
 import org.jboss.deployers.vfs.spi.deployer.AbstractSimpleVFSRealDeployer;
 import org.jboss.deployers.vfs.spi.structure.VFSDeploymentUnit;
+import org.jboss.kernel.Kernel;
 import org.jboss.ruby.runtime.DefaultRubyRuntimeFactory;
 import org.jboss.ruby.runtime.RubyDynamicClassLoader;
 import org.jboss.ruby.runtime.RubyRuntimeFactory;
@@ -13,9 +14,19 @@ import org.jboss.ruby.runtime.metadata.RubyRuntimeMetaData;
 
 public class RubyRuntimeFactoryDeployer extends AbstractSimpleVFSRealDeployer<RubyRuntimeMetaData> {
 
+	private Kernel kernel;
+
 	public RubyRuntimeFactoryDeployer() {
 		super(RubyRuntimeMetaData.class);
 		setStage(DeploymentStages.CLASSLOADER);
+	}
+	
+	public void setKernel(Kernel kernel) {
+		this.kernel = kernel;
+	}
+	
+	public Kernel getKernel() {
+		return this.kernel;
 	}
 
 	@Override
@@ -24,6 +35,7 @@ public class RubyRuntimeFactoryDeployer extends AbstractSimpleVFSRealDeployer<Ru
 		log.trace("creating RubyRuntimeFactory: " + factoryName);
 
 		DefaultRubyRuntimeFactory factory = new DefaultRubyRuntimeFactory(metaData.getLoadPath().getPaths(), metaData.getInitScript());
+		factory.setKernel( this.kernel );
 
 		try {
 			RubyDynamicClassLoader classLoader = createClassLoader(unit);
