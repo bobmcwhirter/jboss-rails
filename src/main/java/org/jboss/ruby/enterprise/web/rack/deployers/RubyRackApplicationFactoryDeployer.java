1277b4b14f933c68b5f9ce56f02288b851e7f45d
Bob McWhirter <bob@fnokd.com>
2009-03-05 09:33:24 -0500

    Add license headers. Remove unused class. Reorganize CXF logging util classes.

--- src/main/java/org/jboss/ruby/enterprise/web/rack/deployers/RubyRackApplicationFactoryDeployer.java
+++ src/main/java/org/jboss/ruby/enterprise/web/rack/deployers/RubyRackApplicationFactoryDeployer.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.ruby.enterprise.web.rack.deployers;
 
 import org.jboss.beans.metadata.spi.BeanMetaData;
@@ -17,7 +38,7 @@ import org.jboss.ruby.runtime.RubyRuntimeFactory;
 public class RubyRackApplicationFactoryDeployer extends AbstractSimpleVFSRealDeployer<RubyRackApplicationMetaData> {
 
 	private static final Logger log = Logger.getLogger(RubyRackApplicationFactoryDeployer.class);
-	
+
 	public RubyRackApplicationFactoryDeployer() {
 		super(RubyRackApplicationMetaData.class);
 		addOutput(BeanMetaData.class);
@@ -26,26 +47,27 @@ public class RubyRackApplicationFactoryDeployer extends AbstractSimpleVFSRealDep
 	@Override
 	public void deploy(VFSDeploymentUnit unit, RubyRackApplicationMetaData metaData) throws DeploymentException {
 		String beanName = "jboss.rack.app." + unit.getSimpleName();
-	
-		log.debug( "deploying rack app factory: " + beanName );
-		
+
+		log.debug("deploying rack app factory: " + beanName);
+
 		RubyRuntimeFactory factory = unit.getAttachment(RubyRuntimeFactory.class);
-		
-		BeanMetaDataBuilder builder = BeanMetaDataBuilder.createBuilder( beanName, RubyRackApplicationFactory.class.getName() );
-		builder.addPropertyMetaData( "rubyRuntimeFactory", factory );
-		builder.addPropertyMetaData( "rackUpScript", metaData.getRackUpScript() );
-		
+
+		BeanMetaDataBuilder builder = BeanMetaDataBuilder.createBuilder(beanName, RubyRackApplicationFactory.class.getName());
+		builder.addPropertyMetaData("rubyRuntimeFactory", factory);
+		builder.addPropertyMetaData("rackUpScript", metaData.getRackUpScript());
+
 		BeanMetaData beanMetaData = builder.getBeanMetaData();
-		
-		unit.addAttachment( BeanMetaData.class.getName() + "$" + RackApplicationFactory.class.getName() + "$" + unit.getName(), beanMetaData, BeanMetaData.class );
-		
-		builder = BeanMetaDataBuilder.createBuilder( beanName + ".pool", SharedRackApplicationPool.class.getName() );
-		ValueMetaData appFactoryInjection = builder.createInject( beanName );
-		builder.addConstructorParameter( RackApplicationFactory.class.getName(), appFactoryInjection );
-		
+
+		unit
+				.addAttachment(BeanMetaData.class.getName() + "$" + RackApplicationFactory.class.getName() + "$" + unit.getName(), beanMetaData, BeanMetaData.class);
+
+		builder = BeanMetaDataBuilder.createBuilder(beanName + ".pool", SharedRackApplicationPool.class.getName());
+		ValueMetaData appFactoryInjection = builder.createInject(beanName);
+		builder.addConstructorParameter(RackApplicationFactory.class.getName(), appFactoryInjection);
+
 		beanMetaData = builder.getBeanMetaData();
-		
-		unit.addAttachment( BeanMetaData.class.getName() + "$" + RackApplicationPool.class.getName() + "$" + unit.getName(), beanMetaData);
+
+		unit.addAttachment(BeanMetaData.class.getName() + "$" + RackApplicationPool.class.getName() + "$" + unit.getName(), beanMetaData);
 	}
 
 }
